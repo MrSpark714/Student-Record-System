e30c4f86dac8e358f1c413a366a42882ced904b0
MrSpark714 <sohaibking714@gmail.com>
2025-04-22 10:55:33 -0700

    Make Class

--- project.cpp
+++ project.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
 
+fstream Mian_file("datafile.csv",std::ios::app);
+
+class Student{
+    private:
+        string name,Class;
+        int age;
+        double marks;
+        char section;
+        static int unique_id;
+    public:
+        string Get_name(){
+            return name;
+        }
+
+};
 int main(){
-    std::cout<<"Hello Pakistan";
+    cout<<"Pakistan";
 }
