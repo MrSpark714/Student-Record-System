9ca446b4320e591560f032abb922d712cc16b469
MrSpark714 <sohaibking714@gmail.com>
2025-04-23 19:46:18 -0700

    Make getter and Setter

--- project.cpp
+++ project.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<string>
-#include<fstream>
+// #include<fstream>
 using namespace std;
 
-fstream Mian_file("datafile.csv",std::ios::app);
+// fstream Mian_file("datafile.csv",std::ios::app);
 
 class Student{
     private:
@@ -12,12 +12,55 @@ class Student{
         double marks;
         char section;
         static int unique_id;
+        int id;
     public:
+        // Here Getter function 
         string Get_name(){
             return name;
         }
+        int Get_age(){
+            return age;
+        }
+        double Get_marks(){
+            return marks;
+        }
+        char Get_section(){
+            return section;
+        }
+        int Get_id(){
+            return id;
+        }
+        // Here Setter function 
+        void Set_name(string name){
+             this->name = name;
+        }
+        void Set_age(int age){
+            if(age >=1){
+                this->age = age;
+            }
+            else{
+                cout<<"Invalid Input! Try Again: ";
+                return;
+            }
+        }
+        void Set_marks(double marks){
+            if(marks >= 0 || marks <= 100){
+                this->marks =marks;
+            }
+            else{
+                cout<<"Invalid Input! Try Again: ";
+                return;
+            }
+        }
+
+
 
 };
+Student :: Student(){ // defualt Constructor 
+    unique_id = 1;
+    id = unique_id;
+    unique_id++;
+}
 int main(){
-    cout<<"Pakistan";
+    Student s1();
 }
